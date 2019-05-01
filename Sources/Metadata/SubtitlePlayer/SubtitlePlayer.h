7ab3642ba16499df4fc3338c021b08997ebe3a59
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2019-05-01 04:01:56 +0200

    Add SubtitlePlayer for platforms

---
+++ Sources/Metadata/SubtitlePlayer/SubtitlePlayer.h
@@ -0,0 +1,19 @@
+//
+//  SubtitlePlayer.h
+//  SubtitlePlayer
+//
+//  Created by Luiz Rodrigo Martins Barbosa on 01.05.19.
+//  Copyright © 2019 Luiz Rodrigo Martins Barbosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SubtitlePlayer.
+FOUNDATION_EXPORT double SubtitlePlayerVersionNumber;
+
+//! Project version string for SubtitlePlayer.
+FOUNDATION_EXPORT const unsigned char SubtitlePlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubtitlePlayer/PublicHeader.h>
+
+
