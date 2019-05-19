d57f689fad360a2a0155d1f4ea769e04e2781f98
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2019-05-19 14:04:45 +0200

    Transport controls

--- Sources/SubtitleApp/watchOS/Player/PlayerViewController.swift
+++ Sources/SubtitleApp/watchOS/Player/PlayerViewController.swift
@@ -1,25 +1,49 @@
 import WatchKit
 
 public final class PlayerViewController: WKInterfaceController {
-    @IBOutlet weak var subtitleLabel: WKInterfaceLabel!
+    @IBOutlet private weak var rewindButton: WKInterfaceButton!
+    @IBOutlet private weak var playToggleButton: WKInterfaceButton!
+    @IBOutlet private weak var forwardButton: WKInterfaceButton!
+    @IBOutlet private weak var subtitleLabel: WKInterfaceLabel!
+
     private var didAppearSignal: (() -> Void)!
     private var willDisappearSignal: (() -> Void)!
+    private var rewindButtonTapSignal: (() -> Void)!
+    private var playToggleButtonTapSignal: (() -> Void)!
+    private var forwardButtonTapSignal: (() -> Void)!
 
     public override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         let viewModel: ViewModel<PlayerViewModelInput, PlayerViewModelOutput>! = InterfaceControllerContext.wrapped(context: context)
 
         let outputs: PlayerViewModelOutput = (
-            subtitle: subtitleLabel.setText,
-            playing: { _ in }
+            subtitle: { [weak self] text in self?.subtitleLabel.setText(text) },
+            playingToggleText: { [weak self] text in self?.playToggleButton.setTitle(text) },
+            rewindButtonHidden: { [weak self] hidden in self?.rewindButton.setHidden(hidden) },
+            forwardButtonHidden: { [weak self] hidden in self?.forwardButton.setHidden(hidden) }
         )
 
         let inputs = viewModel.bind(outputs)
 
         self.didAppearSignal = inputs.didAppear
         self.willDisappearSignal = inputs.willDisappear
+        self.rewindButtonTapSignal = inputs.rewindButtonTap
+        self.playToggleButtonTapSignal = inputs.playToggleButtonTap
+        self.forwardButtonTapSignal = inputs.forwardButtonTap
 
         inputs.awakeWithContext(context)
+    }
+
+    @IBAction func rewindButtonTap() {
+        rewindButtonTapSignal()
+    }
+
+    @IBAction func playToggleButtonTap() {
+        playToggleButtonTapSignal()
+    }
+
+    @IBAction func forwardButtonTap() {
+        forwardButtonTapSignal()
     }
 
     public override func didAppear() {
--- Sources/SubtitleApp/watchOS/Player/PlayerViewModel.swift
+++ Sources/SubtitleApp/watchOS/Player/PlayerViewModel.swift
@@ -6,29 +6,65 @@ import SubtitlePlayer
 public typealias PlayerViewModelInput = (
     awakeWithContext: (Any?) -> Void,
     didAppear: () -> Void,
-    willDisappear: () -> Void
+    willDisappear: () -> Void,
+    rewindButtonTap: () -> Void,
+    playToggleButtonTap: () -> Void,
+    forwardButtonTap: () -> Void
 )
 
 public typealias PlayerViewModelOutput = (
     subtitle: (String) -> Void,
-    playing: (Bool) -> Void
+    playingToggleText: (String) -> Void,
+    rewindButtonHidden: (Bool) -> Void,
+    forwardButtonHidden: (Bool) -> Void
 )
 
 public func playerViewModel(router: Router, subtitle: Subtitle) -> (PlayerViewModelOutput) -> PlayerViewModelInput {
     return { output in
-        let disposeBag = DisposeBag()
+        var disposeBag: DisposeBag? = DisposeBag()
+        var playing = false
+        var currentLine = 0
+
         return (
             awakeWithContext: { _ in output.subtitle("") },
             didAppear: {
-                SubtitlePlayer
-                    .play(subtitle: subtitle)
-                    .subscribe(onNext: { lines in
-                        DispatchQueue.main.async {
-                            output.subtitle(lines.map(^\.text).joined(separator: "\n"))
-                        }
-                    }).disposed(by: disposeBag)
+                setPlaying(playing, output: output)
             },
-            willDisappear: { }
+            willDisappear: { },
+            rewindButtonTap: {
+                currentLine = max(currentLine - 1, 0)
+                output.subtitle(subtitle.lines.first(where: { $0.sequence == currentLine }).map(^\.text) ?? "")
+            },
+            playToggleButtonTap: {
+                playing.toggle()
+                setPlaying(playing, output: output)
+
+                if playing {
+                    disposeBag = disposeBag ?? DisposeBag()
+                    currentLine = currentLine >= subtitle.lines.count ? 0 : currentLine
+                    return SubtitlePlayer
+                        .play(subtitle: subtitle, from: currentLine)
+                        .subscribe(onNext: { lines in
+                            DispatchQueue.main.async {
+                                currentLine = lines.last?.sequence ?? currentLine
+                                output.subtitle(lines.map(^\.text).joined(separator: "\n"))
+                            }
+                        }).disposed(by: disposeBag!)
+                } else {
+                    disposeBag = nil
+                    output.subtitle(subtitle.lines.first(where: { $0.sequence == currentLine }).map(^\.text) ?? "")
+                }
+            },
+            forwardButtonTap: {
+                currentLine = min(currentLine + 1, subtitle.lines.map(^\.sequence).max() ?? Int.max)
+                output.subtitle(subtitle.lines.first(where: { $0.sequence == currentLine }).map(^\.text) ?? "")
+            }
         )
     }
+}
+
+private func setPlaying(_ playing: Bool, output: PlayerViewModelOutput) {
+    output.forwardButtonHidden(playing ? true : false)
+    output.rewindButtonHidden(playing ? true : false)
+    output.playingToggleText(playing ? "⏸" : "▶️")
 }
